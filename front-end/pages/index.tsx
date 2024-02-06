fde28d133e33a023babacd3ce1cb976a6d9ff004
NiteshK90 <nitesh@mynextdeveloper.com>
2024-02-06 11:15:51 +0530

    Fix: removed pre installed folders

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,13 +1,10 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
-
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <html>
+    <head>Page</head>
+    <body>
+      <div>Hello world</div>
+    </body>
+  </html>
 );
 
 export default IndexPage;
