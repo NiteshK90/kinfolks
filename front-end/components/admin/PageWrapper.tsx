865a481c6085fb252fe02ac4828ef18b16293a37
NiteshK90 <nitesh@mynextdeveloper.com>
2024-02-27 11:06:57 +0530

    Feat: tailwind added

--- front-end/components/admin/PageWrapper.tsx
+++ front-end/components/admin/PageWrapper.tsx
@@ -3,7 +3,9 @@ import { ReactNode } from "react";
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div>
-      <header className="text-center">Admin</header>
+      <header className="text-center py-4">
+        <h1>Admin</h1>
+      </header>
       <div>
         <div></div>
         <div>{children}</div>
