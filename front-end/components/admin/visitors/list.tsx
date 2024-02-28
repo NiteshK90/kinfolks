7d3571890f8c4e4b05f6c7def57b1664f31d991a
NiteshK90 <nitesh@mynextdeveloper.com>
2024-02-28 11:34:54 +0530

    Feat: visitor's list CSS

--- front-end/components/admin/visitors/list.tsx
+++ front-end/components/admin/visitors/list.tsx
@@ -10,24 +10,34 @@ export const List = () => {
   }
   return (
     <div>
-      <div className="grid grid-cols-6">
-        <div>Sr.</div>
-        <div>Name</div>
-        <div>Mobile</div>
-        <div>Email</div>
-        <div></div>
-        <div>When to visit</div>
+      <div className="p-4">
+        <h3>Below is list of visitors</h3>
       </div>
-      {data.map((item, index) => (
-        <div key={`visitor-row-${index}`} className="grid grid-cols-6">
-          <div>{index + 1}</div>
-          <div>{item.name}</div>
-          <div>{item.mobile}</div>
-          <div>{item.email}</div>
-          <div></div>
-          <div>{item.whenToVisit}</div>
+      <div className="m-4 border border-b-0">
+        <div className="grid grid-cols-6 border-b font-semibold">
+          <div className="p-2">Sr.</div>
+          <div className="p-2">Name</div>
+          <div className="p-2">Mobile</div>
+          <div className="p-2">Email</div>
+          <div className="p-2">When to visit</div>
+          <div className="p-2">Action</div>
         </div>
-      ))}
+        <div className="overflow-auto">
+          {data.map((item, index) => (
+            <div
+              key={`visitor-row-${index}`}
+              className="grid grid-cols-6 border-b"
+            >
+              <div className="p-2">{index + 1}</div>
+              <div className="p-2">{item.name}</div>
+              <div className="p-2">{item.mobile}</div>
+              <div className="p-2">{item.email}</div>
+              <div className="p-2">{item.whenToVisit}</div>
+              <div className="p-2"></div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
