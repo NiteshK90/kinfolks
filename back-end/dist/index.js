4454d1d58bd4cc56063985ed328df74ec46aa2e2
NiteshK90 <nitesh@mynextdeveloper.com>
2024-03-21 12:10:58 +0530

    Fix: uninstalled unwanted libraries

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -9,8 +9,6 @@ const Visitors_1 = __importDefault(require("./src/routes/Visitors"));
 const Users_1 = __importDefault(require("./src/routes/Users"));
 const app = (0, express_1.default)();
 const port = 8000;
-const DB_URL = process.env.DATABASE_URL ||
-    "postgresql://justmac@localhost:5432/kinfolks?schema=public&connection_limit=5&pool_timeout=0";
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 //All routes-
--- back-end/index.ts
+++ back-end/index.ts
@@ -1,14 +1,10 @@
 import express, { Express } from "express";
 import cors from "cors";
-import mongoose, { Error } from "mongoose";
 import visitorsRoutes from "./src/routes/Visitors";
 import usersRoutes from "./src/routes/Users";
 
 const app: Express = express();
 const port = 8000;
-const DB_URL =
-  process.env.DATABASE_URL ||
-  "postgresql://justmac@localhost:5432/kinfolks?schema=public&connection_limit=5&pool_timeout=0";
 
 app.use(express.json());
 app.use(cors());
