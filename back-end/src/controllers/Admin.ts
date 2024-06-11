d37326ce09b2271315e1c08a7f27870b88a028b2
NiteshK90 <nitesh@mynextdeveloper.com>
2024-06-11 14:54:44 +0530

    Fix: secret key

--- back-end/src/controllers/Admin.ts
+++ back-end/src/controllers/Admin.ts
@@ -5,6 +5,8 @@ import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
+const secretKey = process.env.JWT_SECRET;
+
 export const updatePassword = async (req: Request, res: Response) => {
   const { id, password } = req.body;
   const salt = await bcrypt.genSalt(10);
@@ -31,7 +33,7 @@ export const login = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Wrong username or password" });
   }
 
-  const token = jwt.sign({ userId: user.id }, "your_secret_key", {
+  const token = jwt.sign({ userId: user.id }, secretKey || "", {
     expiresIn: "1h",
   });
   res.status(201).json({ message: "Login successful", token: token });
